e8116a883dad24970cbb04095d46b3824bdefdfe
evgeny <krotoveann@gmail.com>
2021-05-10 20:41:55 +0300

    News for iOS fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:covid_app/news/news_homepage.dart';
 import 'package:covid_app/login_signup_page.dart';
-import 'package:covid_app/quiz_page.dart';
 import 'package:flutter/material.dart';
-import 'package:covid_app/home_screen.dart';
-
-import 'login_info.dart';
 
 void main() => runApp(MyApp());
 
